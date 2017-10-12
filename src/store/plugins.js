4ef459626fab7a0036f2c5ebee50bd5a0aee0ced
santhisenan <santhisenan@outlook.com>
2017-10-12 07:03:59 +0530

    minor change in local forage data updatation

--- src/store/plugins.js
+++ src/store/plugins.js
@@ -1,10 +1,12 @@
 import localforage from 'localforage'
 
 export const loadFromCache = (store) => {
-  localforage.getItem('username').then((value) => {
-  if(value){
-    store.commit('loadUsernameFromCache', value);
-  }
-})
+  if(!store.state.username){
+    localforage.getItem('username').then((value) => {
+    if(value){
+      store.commit('loadUsernameFromCache', value);
+    }
+  })
 .catch((err) => {throw err})
+}
 }
